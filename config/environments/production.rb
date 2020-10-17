72b19b43d4d3002830d2e51f73cb0a90b9d158c0
BachShinichiroe <kitchen.number@gmail.com>
2020-10-17 22:23:22 +0900

    [fix]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
       port:           587,
       user_name:      ENV['GMAIL_USERNAME'],
       password:       ENV['GMAIL_PASSWORD'],
-      domain:         'heroku.com',
+      domain:         'herokuapp.com',
       enable_starttls_auto: true,
       authentication: :plain
   }
